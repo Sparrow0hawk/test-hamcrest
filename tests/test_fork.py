dd36a14ee047b8921564dccb1c7f37b30f9ea9e0
Sparrow0hawk <alexjcoleman@gmail.com>
2023-11-17 21:30:10 +0000

    Simplify test and add vanilla comparison

--- tests/test_fork.py
+++ tests/test_fork.py
@@ -1,8 +1,15 @@
 from hamcrest import assert_that, equal_to
+
 from test_hamcrest import Fork
 
 
-def test_forks_have_same_name():
+def test_forks_have_same_name_hamcrest():
     small_fork = Fork("small")
-    big_fork = Fork("small")
+
     assert_that(small_fork.name, equal_to("small"))
+
+
+def test_forks_have_same_name_vanilla():
+    small_fork = Fork("small")
+
+    assert small_fork.name == "small"
