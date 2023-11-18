b235377e4c9e15e9448c14870f3f76dcdbe8efe5
Sparrow0hawk <alexjcoleman@gmail.com>
2023-11-18 15:25:57 +0000

    Testing new instances with Hamcrest fails

--- tests/test_plate.py
+++ tests/test_plate.py
@@ -1,3 +1,4 @@
+import pytest
 from hamcrest import assert_that, equal_to, has_properties
 
 from test_hamcrest import Fork, Knife
@@ -9,9 +10,15 @@ def test_plate() -> None:
     knife = Knife("large")
     plate = Plate(meal="Dinner", left=fork, right=knife)
 
-    assert_that(plate, has_properties(
-        meal=equal_to("Dinner"),
-        left=equal_to(fork),
-        right=equal_to(knife)
-        )
+    assert_that(plate, has_properties(meal=equal_to("Dinner"), left=equal_to(fork), right=equal_to(knife)))
+
+
+@pytest.mark.xfail
+def test_plate_fails_with_different_instance() -> None:
+    fork = Fork("small")
+    knife = Knife("large")
+    plate = Plate(meal="Dinner", left=fork, right=knife)
+
+    assert_that(
+        plate, has_properties(meal=equal_to("Dinner"), left=equal_to(Fork("small")), right=equal_to(Knife("large")))
     )
